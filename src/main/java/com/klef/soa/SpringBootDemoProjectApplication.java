6dd5cc2ff8eff4f72664d22df20d00db6f6bcfa5
SushmaYelnedi <2400031778@kluniversity.in>
2026-07-23 17:11:26 +0530

    project Version v1.0

---
+++ src/main/java/com/klef/soa/SpringBootDemoProjectApplication.java
@@ -0,0 +1,15 @@
+package com.klef.soa;
+
+import org.hibernate.internal.build.AllowSysOut;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoProjectApplication.class, args);
+	System.out.println("Project is running");
+	}
+
+}
